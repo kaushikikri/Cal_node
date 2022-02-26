266ca7e6cda972e56ae370010127435e68a41c9b
kaushikikri <kaushikumari03@gmail.com>
2022-02-26 13:15:07 +0530

    Single screen calculator

---
+++ app.js
@@ -0,0 +1,62 @@
+const { create, evaluateDependencies } = require('mathjs');
+const express = require('express');
+const bodyparse = require('body-parser');
+const app = express();
+
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+const mod = (a, b) => a % b;
+
+const math = create(evaluateDependencies);
+math.import({ add, subtract, multiply, divide, mod }, { override: true });
+
+
+// connecting to db
+const client = require('./db');
+client.connect();
+
+
+// getting form value
+app.use(bodyparse.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+
+app.get('', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+app.post('/resl', (req, res) => {
+    var num = req.body.num1;
+    var result = math.evaluate(num).toString();
+    res.send(result);
+    client.query("Insert into calc values('" + num + "','" + result + "',current_timestamp)",
+        (err, result) => {
+            if (!err) {
+                console.log('Inserted 1 Row!');
+            }
+            else {
+                console.log(err);
+            }
+            // client.end();
+        });
+});
+
+
+app.get('/hs', (req, res) => {
+    client.query("Select * from calc ", (err, result) => {
+        if (!err) {
+            res.render('hs', { 'items': result.rows }
+            );
+            // client.end();
+        }
+        else {
+            console.log(err);
+        }
+    })
+
+});
+
+
+app.listen(4000, (res) => {
+    console.log('Connected at port 4000');
+});
